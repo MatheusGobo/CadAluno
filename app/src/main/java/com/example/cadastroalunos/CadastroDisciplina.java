efdc8fbf328640ec6ecb6bddb7906bdc58761614
Matheus Gobo <matheusgobo900@gmail.com>
2022-03-12 10:10:39 -0300

    Criada activity Disciplina

---
+++ app/src/main/java/com/example/cadastroalunos/CadastroDisciplina.java
@@ -0,0 +1,13 @@
+package com.example.cadastroalunos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroDisciplina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_disciplina);
+    }
+}
